65b6a2af19d5e4ab4d57f290df6eaf487ca92c25
Prabhat Chandra <prabhatc@gmail.com>
2024-07-02 11:43:24 +0530

    Added rest api example
    Added rest api example

--- day15/restapiapp/app/customerapp/views.py
+++ day15/restapiapp/app/customerapp/views.py
@@ -3,7 +3,16 @@ from django.http import Http404
 from django.db.models import F
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, Customers. You're at the customer app page.")
+    
+   
+
+# Create your views here.
+@api_view(['GET'])
+def getData(request):
+return Response()
